72ca5d1f6cb52dc980d77c955ca4ebf374b2267d
aakashbathla <aakash.bathla7@gmail.com>
2022-03-08 19:02:44 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [arr, setArr] = useState([
+    ["", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""],
+  ]);
+  const [initialValue, setInitialValue] = useState(0);
+  const [checkColor, setCheckColor] = useState([]);
+  function handleClick(key, key2) {
+    let newArr = [...arr];
+    newArr[key][key2] = "Box" + initialValue;
+    let newCheckcolor = [...checkColor];
+    if (newCheckcolor.length < 2) {
+      newCheckcolor.push([key, key2]);
+    } else {
+      newCheckcolor.shift();
+      newCheckcolor.push([key, key2]);
+    }
+    console.log(newCheckcolor);
+    setCheckColor(newCheckcolor);
+    setArr(newArr);
+    setInitialValue(initialValue + 1);
+  }
+  function getClass(key, key2) {
+    let a = "";
+    if (checkColor.find((el) => el[0] === key && el[1] === key2)) {
+      a = "red";
+    } else {
+      a = "blue";
+    }
+    return a;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {arr.map((val, key) => (
+        <div className="flex">
+          {val.map((val2, key2) => (
+            <div
+              className={getClass(key, key2)}
+              onClick={() => handleClick(key, key2)}
+            >
+              {val2}
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
